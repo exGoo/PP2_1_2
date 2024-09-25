ef1c7272f84e1b13fed4b9c651a98b913bb32523
Vladislav Guliaev <nascentesxxx@gmail.com>
2024-09-25 06:16:52 +0400

    Complete task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,11 +2,20 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
+
     public static void main(String[] args) {
+
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld beanHello1 = (HelloWorld) applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld beanHello2 = applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat", Cat.class);
+        Cat beanCat2 = (Cat) applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(beanHello1 == beanHello2);
+        System.out.println(beanCat1 == beanCat2);
     }
+
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,20 @@
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cat {
+
+    public Cat() {
+
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
